be76ed3d5d37e17803dad4eacff02e8d689246d1
lucashflores <lucas.henrique.flores2001@gmail.com>
2023-10-13 15:20:54 -0300

    game is done, missing tests

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,9 +1,10 @@
 defmodule ExMon do
   alias ExMon.Player
   alias ExMon.Game
-  alias ExMon.Game.{Status,Actions}
+  alias ExMon.Game.{Status, Actions}
 
   @computer_name "Robotnik"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
@@ -14,10 +15,39 @@ defmodule ExMon do
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
 
-    Status.print_round_message()
+    Status.print_round_message(Game.info())
   end
 
   def make_move(move) do
-    Actions.fetch_move(move)
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
   end
+
+  defp handle_status(:game_over, move), do: Status.print_round_message(Game.info())
+  defp handle_status(_other, move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+
+    computer_move(Game.info())
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> Actions.heal()
+      move -> Actions.attack(move)
+    end
+
+    Status.print_round_message(Game.info())
+  end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    {:ok, Enum.random(@computer_moves)}
+    |> do_move()
+  end
+
+  defp computer_move(_), do: :ok
 end
--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,9 +1,25 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.{Attack, Heal}
+
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
     |> find_move(move)
+  end
+
+  def attack(move) do
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
+  end
+
+  def heal() do
+    case Game.turn() do
+      :player -> Heal.heal_life(:player)
+      :computer -> Heal.heal_life(:computer)
+    end
   end
 
   defp find_move(moves, move) do
